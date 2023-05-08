21ec3142cd24c82296320b96c94e7518e20a1145
galit.moss <galitmoss@me.com>
2023-05-09 08:23:52 +1000

    added basic html files and rendered templates to test

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 import os
 import psycopg2
 
@@ -7,12 +7,20 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     # connection = psycopg2.connect(host=os.getenv("PGHOST"), user=os.getenv("PGUSER"), password=os.getenv("PGPASSWORD"), port=os.getenv("PGPORT"), dbname=os.getenv("PGDATABASE"))
-    connection = psycopg2.connect(os.getenv("DATABASE_URL"))
+    connection = psycopg2.connect(os.getenv("postgres://pg:pSyCdeo1JQVO7wzGZm2wCEUOUXb5Axo9@dpg-ch8fge5gk4q7lmq3l460-a.oregon-postgres.render.com/project2_8x9r"))
     cursor = connection.cursor()
     cursor.execute("SELECT * FROM mytable;")
     results = cursor.fetchall()
     connection.close()
     return f"{results[0]}"
+
+@app.route('/home')
+def homepage():
+    return render_template("home.html")
+
+@app.route('/messages')
+def messages():
+    return render_template("messages.html")
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
---
+++ table.sql
@@ -0,0 +1,31 @@
+CREATE TABLE users(id SERIAL PRIMARY KEY, username TEXT NOT NULL, user_pwd TEXT NOT NULL);
+DROP TABLE users;
+
+CREATE TABLE images(id SERIAL PRIMARY KEY, img_url TEXT NOT NULL, text_desc TEXT NOT NULL, img_year INTEGER NOT NULL);
+DROP TABLE images;
+
+
+INSERT INTO users(name, username, user_pwd) VALUES (%s, %s);
+
+
+
+INSERT INTO images(name, img_url, text_desc, img_year) VALUES (%s, %s, %s);
+<img src="https://i.ibb.co/yPSjJyX/c91ca8b9-eb4f-4521-a86e-04694a50f256.jpg"> 2022, Janine Ziva at someones 90th
+<img src="https://i.ibb.co/NNVbbKm/H-D-Wedding-01605.jpg"> 2021 Hannah wedding me Gabi Ziva Hannah 
+<img src="https://i.ibb.co/sj70p5F/H-D-Wedding-01242.jpg"> 2021 Hannah & Ziva
+<img src="https://i.ibb.co/pntF5Kg/64914e03-6215-46b7-8ad8-fc1c9cf209f5.jpg"> 2019 Ziva Isa me
+<img src="https://i.ibb.co/HKzsK9v/136df341-fd11-47af-a1dd-6c31704c39fd.jpg"> 2019 Ziva Fudge in trolley
+<img src="https://i.ibb.co/Ks39Mgw/e0712f96-2b91-4ae3-94e9-42bcb50f3aec.jpg"> 2018 Ziva Joel Opera house
+<img src="https://i.ibb.co/cDpgTSW/IMG-4820.jpg"> 2018 Ziva Granny me at airport
+<img src="https://i.ibb.co/QrV3c4k/5aab7870-7a73-40bb-88eb-832693ff7568.jpg"> 2018 Ziva baby Ashi selfie
+<img src="https://i.ibb.co/R4Kk1j9/IMG-4091.jpg"> 2015 my Graduation Ziva Joel me
+<img src="https://i.ibb.co/KxmWQ4Q/54-CC02-E8-D493-4931-A6-D0-7-A3256-AB16-A0.jpg"> 2020 Ziva and I boat selfie
+<img src="https://i.ibb.co/M7MVHwx/b91bd789-92e9-4760-ad6b-9e28319e026f.jpg"> 2023 Ziva Aurie
+<img src="https://i.ibb.co/Mgj48xS/ziva-joel-on-couch.jpg"> 2019 ziva joel on couch
+<img src="https://i.ibb.co/1f36gXg/ziva-granny.jpg"> 2020 ziva and granny
+<img src="https://i.ibb.co/RhfQs16/kurr-fam.jpg"> 2022 kurr family ziva me liat
+<img src="https://i.ibb.co/g7bVj6d/alon-ziva.jpg"> 2022 alon-ziva
+<img src="https://i.ibb.co/NpdQcSh/ziva-baby-outfit.jpg"> 2023 ziva baby outfit
+<img src="https://i.ibb.co/g6s6LdM/ziva-liat-vineyard.jpg"> 2020 ziva-liat-vineyard
+<img src="https://i.ibb.co/5knpZ0r/fam-on-boat.jpg"> 2019 fam-on-boat
+<img src="https://i.ibb.co/gDKRjc0/alons-barmi.jpg"> 2011 alons-barmi
