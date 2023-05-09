d67d9f37dcfa58b9996d084b2b13d99f1824fee8
galit.moss <galitmoss@me.com>
2023-05-09 12:12:34 +1000

    forgot dictionary command previously

--- models/messages.py
+++ models/messages.py
@@ -1,11 +1,15 @@
 import psycopg2
 from models import common
 
+def convert_to_dictionary(item):
+    return {"id": str(item[0]), "user_msg": item[1]}
+
 def insert_message(user_msg):
     common.sql_write("INSERT INTO messages (user_msg) VALUES (%s);", [user_msg])
 
 def get_message(id):
-    common.sql_read("SELECT * FROM messages WHERE id=%s", [id])[0]
+    item = common.sql_read("SELECT * FROM messages WHERE id=%s", [id])[0]
+    return convert_to_dictionary(item)
 
 def edit_message(id, user_msg):
     common.sql_write(f"UPDATE messages SET user_msg=%s WHERE id={id}", [user_msg])
