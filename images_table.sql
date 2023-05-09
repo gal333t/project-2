b778ecdbc3e57d08e5f2da32920ee6d701b17aa9
galit.moss <galitmoss@me.com>
2023-05-09 21:29:30 +1000

    Updating table data

---
+++ images_table.sql
@@ -0,0 +1,26 @@
+
+
+INSERT INTO images(img_url, text_desc, img_year) VALUES ('https://i.ibb.co/Ks39Mgw/e0712f96-2b91-4ae3-94e9-42bcb50f3aec.jpg', 'Ziva and Joel at the Opera House', 2018);
+INSERT INTO images(img_url, text_desc, img_year) VALUES ('https://i.ibb.co/cDpgTSW/IMG-4820.jpg', 'Ziva, Granny and Galit at the airport before flying to Israel', 2018);
+INSERT INTO images(img_url, text_desc, img_year) VALUES ('https://i.ibb.co/QrV3c4k/5aab7870-7a73-40bb-88eb-832693ff7568.jpg', 'Ziva and baby Ashi', 2018);
+INSERT INTO images(img_url, text_desc, img_year) VALUES ('https://i.ibb.co/R4Kk1j9/IMG-4091.jpg', 'Ziva and Joel with Galit at her graduation', 2018);
+
+INSERT INTO images(img_url, text_desc, img_year) VALUES ('https://i.ibb.co/Mgj48xS/ziva-joel-on-couch.jpg', 'Ziva and Joel on the couch', 2019);
+INSERT INTO images(img_url, text_desc, img_year) VALUES ('https://i.ibb.co/pntF5Kg/64914e03-6215-46b7-8ad8-fc1c9cf209f5.jpg', 'Ziva, Isa and me at a walk in Melbourne', 2019);
+INSERT INTO images(img_url, text_desc, img_year) VALUES ('https://i.ibb.co/HKzsK9v/136df341-fd11-47af-a1dd-6c31704c39fd.jpg', 'Ziva and Fudge in a trolley', 2019);
+
+INSERT INTO images(img_url, text_desc, img_year) VALUES ('https://i.ibb.co/5knpZ0r/fam-on-boat.jpg', 'Everyone on the boat for whale watching', 2020);
+INSERT INTO images(img_url, text_desc, img_year) VALUES ('https://i.ibb.co/KxmWQ4Q/54-CC02-E8-D493-4931-A6-D0-7-A3256-AB16-A0.jpg', 'Ziva and Galit on the boat', 2020);
+INSERT INTO images(img_url, text_desc, img_year) VALUES ('https://i.ibb.co/1f36gXg/ziva-granny.jpg', 'Ziva and granny', 2020);
+INSERT INTO images(img_url, text_desc, img_year) VALUES ('https://i.ibb.co/g6s6LdM/ziva-liat-vineyard.jpg', 'Ziva and Liat in the Mornington Peninsula', 2020);
+
+INSERT INTO images(img_url, text_desc, img_year) VALUES ('https://i.ibb.co/NNVbbKm/H-D-Wedding-01605.jpg', 'Ziva, Hannah, Gabi and Galit', 2021);
+INSERT INTO images(img_url, text_desc, img_year) VALUES ('https://i.ibb.co/NNVbbKm/H-D-Wedding-01242.jpg', 'Hannah and Ziva at Hannah and Dans wedding', 2021);
+
+
+INSERT INTO images(img_url, text_desc, img_year) VALUES ('https://i.ibb.co/RhfQs16/kurr-fam.jpg', 'Kurr family with Galit, Ziva and Liat', 2022);
+INSERT INTO images(img_url, text_desc, img_year) VALUES ('https://i.ibb.co/g7bVj6d/alon-ziva.jpg', 'Ziva and Alon on Mothers Day', 2022);
+
+
+INSERT INTO images(img_url, text_desc, img_year) VALUES ('https://i.ibb.co/M7MVHwx/b91bd789-92e9-4760-ad6b-9e28319e026f.jpg', 'Ziva and Aurie', 2023);
+INSERT INTO images(img_url, text_desc, img_year) VALUES ('https://i.ibb.co/NpdQcSh/ziva-baby-outfit.jpg', 'Ziva and Auries baby outfit', 2023);
--- table.sql
+++ table.sql
@@ -7,32 +7,8 @@ DROP TABLE images;
 CREATE TABLE messages(id SERIAL PRIMARY KEY, username TEXT NOT NULL, user_msg TEXT NOT NULL);
 DROP TABLE messages;
 
-INSERT INTO users(name, username, user_pwd) VALUES (%s, %s);
+INSERT INTO users(username, user_pwd) VALUES (%s, %s);
 
 INSERT INTO messages(username, user_msg) VALUES('Galit', 'Mom, you are the glue that holds our family together, the sunshine on a cloudy day, and the chocolate chips in our cookies!');
 
-
 INSERT INTO images(img_url, text_desc, img_year) VALUES (%s, %s, %s);
-
-INSERT INTO images(img_url, text_desc, img_year) VALUES ('https://i.ibb.co/NNVbbKm/H-D-Wedding-01242.jpg', 'Hannah and Ziva at the wedding', 2021);
-
-
-<img src="https://i.ibb.co/yPSjJyX/c91ca8b9-eb4f-4521-a86e-04694a50f256.jpg"> 2022, Janine Ziva at someones 90th
-<img src="https://i.ibb.co/NNVbbKm/H-D-Wedding-01605.jpg"> 2021 Hannah wedding me Gabi Ziva Hannah 
-<img src="https://i.ibb.co/sj70p5F/H-D-Wedding-01242.jpg"> 2021 Hannah & Ziva
-<img src="https://i.ibb.co/pntF5Kg/64914e03-6215-46b7-8ad8-fc1c9cf209f5.jpg"> 2019 Ziva Isa me
-<img src="https://i.ibb.co/HKzsK9v/136df341-fd11-47af-a1dd-6c31704c39fd.jpg"> 2019 Ziva Fudge in trolley
-<img src="https://i.ibb.co/Ks39Mgw/e0712f96-2b91-4ae3-94e9-42bcb50f3aec.jpg"> 2018 Ziva Joel Opera house
-<img src="https://i.ibb.co/cDpgTSW/IMG-4820.jpg"> 2018 Ziva Granny me at airport
-<img src="https://i.ibb.co/QrV3c4k/5aab7870-7a73-40bb-88eb-832693ff7568.jpg"> 2018 Ziva baby Ashi selfie
-<img src="https://i.ibb.co/R4Kk1j9/IMG-4091.jpg"> 2015 my Graduation Ziva Joel me
-<img src="https://i.ibb.co/KxmWQ4Q/54-CC02-E8-D493-4931-A6-D0-7-A3256-AB16-A0.jpg"> 2020 Ziva and I boat selfie
-<img src="https://i.ibb.co/M7MVHwx/b91bd789-92e9-4760-ad6b-9e28319e026f.jpg"> 2023 Ziva Aurie
-<img src="https://i.ibb.co/Mgj48xS/ziva-joel-on-couch.jpg"> 2019 ziva joel on couch
-<img src="https://i.ibb.co/1f36gXg/ziva-granny.jpg"> 2020 ziva and granny
-<img src="https://i.ibb.co/RhfQs16/kurr-fam.jpg"> 2022 kurr family ziva me liat
-<img src="https://i.ibb.co/g7bVj6d/alon-ziva.jpg"> 2022 alon-ziva
-<img src="https://i.ibb.co/NpdQcSh/ziva-baby-outfit.jpg"> 2023 ziva baby outfit
-<img src="https://i.ibb.co/g6s6LdM/ziva-liat-vineyard.jpg"> 2020 ziva-liat-vineyard
-<img src="https://i.ibb.co/5knpZ0r/fam-on-boat.jpg"> 2019 fam-on-boat
-<img src="https://i.ibb.co/gDKRjc0/alons-barmi.jpg"> 2011 alons-barmi
