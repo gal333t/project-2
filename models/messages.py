ba7f18c0376a96926b5253964129ae2518625f4e
galit.moss <galitmoss@me.com>
2023-05-09 11:31:35 +1000

    added functions for edit and delete

--- models/messages.py
+++ models/messages.py
@@ -3,3 +3,12 @@ from models import common
 
 def insert_message(user_msg):
     common.sql_write("INSERT INTO messages (user_msg) VALUES (%s);", [user_msg])
+
+def get_message(id):
+    common.sql_read("SELECT * FROM messages WHERE id=%s", [id])[0]
+
+def edit_message(id, user_msg):
+    common.sql_write(f"UPDATE messages SET user_msg=%s WHERE id={id}", [user_msg])
+
+def delete_message(id):
+    common.sql_write(f"DELETE FROM messages WHERE id={id}", [id])
