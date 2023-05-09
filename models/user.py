ac751b46e02f626c9089b21cf6ee6aa69988b71e
galit.moss <galitmoss@me.com>
2023-05-09 14:33:43 +1000

    changes made to user functions

--- models/user.py
+++ models/user.py
@@ -1,9 +1,16 @@
-from flask import redirect, render_template, session
+import bcrypt
 from models import common
 
-def get_user(filter_clause, params):
-  results = common.sql_read(f"SELECT * FROM users {filter_clause};", params)
+def get_valid_user(username, plain_text_password):
+  results = common.sql_read(f"SELECT * FROM users WHERE username=%s;", [username])
   if len(results):
     user = results[0]
-    return { "id": user[0], "username": user[1], "user_pwd": user[2] }
+    user_formatted = { "id": user[0], "username": user[1], "password_hash": user[2]}
+    if bcrypt.checkpw(plain_text_password.encode(), user_formatted["password_hash"].encode()):
+      return user_formatted
+    return None
   return None
+
+def add_user(username, plain_text_password):
+  password_hash = bcrypt.hashpw(plain_text_password.encode(), bcrypt.gensalt()).decode()
+  common.sql_write("INSERT INTO users (username, password_hash) VALUES (%s, %s);", [username, password_hash])
